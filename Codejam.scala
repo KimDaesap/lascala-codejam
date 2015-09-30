8f8721ed7ece85ae6050f8a88e36951f9f7006e0
Choi SangMin <sapzil78@gmail.com>
2015-09-30 12:48:55 +0900

    Update Codejam.scala

--- Codejam.scala
+++ Codejam.scala
@@ -1,34 +1,36 @@
 import java.io.PrintWriter
 
 object Codejam {
-  // Config.
-  val inputFilePath = "example.in"
-  val outputFilePath = "example.out"
-  val isConsole = true
 
-  // Main procedure.
-  def main(args: Array[String]): Unit = {
-    val inputLines = scala.io.Source.fromFile(inputFilePath).getLines()
-    val inputCount = inputLines.next.toInt
-    val writer = if (isConsole) new PrintWriter(scala.Console.out)
-      else new PrintWriter(outputFilePath)
+	// Config.
+	val inputFilePath = "example.in"
+	val outputFilePath = "example.out"
+	val isConsole = true
 
-    try {
-      process(inputCount, inputLines)(writer.println)
-    } finally {
-      writer.flush()
-      writer.close()
+	// Main procedure.
+	def main(args: Array[String]): Unit = {
+		val inputLines = scala.io.Source.fromFile(inputFilePath).getLines()
+		val writer = if (isConsole) new PrintWriter(Console.out)
+		else new PrintWriter(outputFilePath)
+
+		try {
+			process(inputLines)(writer.println)
+		} finally {
+			writer.flush()
+			writer.close()
+		}
+	}
+
+	// Process input value.
+	def process(inputLines: Iterator[String])(lineOut: String => Unit) = {
+		val inputCount = inputLines.next.toInt
+
+		for (num <- 1 to inputCount) {
+			val data = inputLines.next()
+			val result = data
+
+			lineOut(s"Case#$num: $result")
+        }
     }
-  }
-
-  // Process input value.
-  def process(inputCount: Int, inputLines: Iterator[String])(lineOut: String => Unit) = {
-    for (num <- 1 to inputCount) {
-      val count = inputLines.next()
-      val value = inputLines.next()
-      val result = value.grouped(8).map(x => {(0 /: x)((acc, x) => {acc << 1 | ~(x >> 1) & 1})}.toChar).mkString
-      lineOut(s"Case #$num: $result")
-    }
-  }
 
 }
