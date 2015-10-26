3015455691ee4e428fbb1d1a91d72def06e16cb2
Choi SangMin <sapzil78@gmail.com>
2015-10-26 16:14:48 +0900

    Update Codejam.scala

--- Codejam.scala
+++ Codejam.scala
@@ -11,11 +11,11 @@ object Codejam {
 	def main(args: Array[String]): Unit = {
 		val inputLines = scala.io.Source.fromFile(inputFilePath).getLines()
 		val writer = if (isConsole) new PrintWriter(Console.out)
-			else new PrintWriter(outputFilePath)
+		else new PrintWriter(outputFilePath)
 
 		try {
 			// 입력 케이스 수 T 만큼 입력 처리.
-			(1 to inputLines.next().toInt).foreach(process(_, inputLines)(writer.println))
+			(1 to inputLines.next().toInt).foreach((num) => writer.println(process(num, inputLines)))
 		}
 		finally {
 			writer.flush()
@@ -24,10 +24,10 @@ object Codejam {
 	}
 
 	// Process input value.
-	def process(num: Int, inputLines: Iterator[String])(lineOut: String => Unit) = {
+	def process(num: Int, inputLines: Iterator[String]): String = {
 		val result = new StringBuilder
 
-		lineOut(s"Case #$num: $result")
+		result.toString
 	}
 
 }
