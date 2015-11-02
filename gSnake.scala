5e38d708d51b84c2a6f9f734d6c2b8fcc86d7067
Choi SangMin <sapzil78@gmail.com>
2015-11-02 17:09:05 +0900

    Large practice는 아직 안풀림.

--- gSnake.scala
+++ gSnake.scala
@@ -1,13 +1,12 @@
 import java.io.PrintWriter
-
-import scala.collection.mutable.Queue
+import scala.collection._
 
 object Codejam {
 	// Config.
-	val inputName = "D-small-practice"
+	val inputName = "D-large-practice"
 	val inputFilePath = inputName + ".in"
 	val outputFilePath = inputName + ".out"
-	val isConsole = false
+	val isConsole = true
 
 	// Main procedure.
 	def main(args: Array[String]): Unit = {
@@ -17,8 +16,7 @@ object Codejam {
 
 		try {
 			// 입력 케이스의 수 T의 횟수만큼 입력 처리.
-			(1 to inputLines.next().toInt).foreach(
-				(num) => writer.println(s"Case #$num: ${process(inputLines)}"))
+			(1 to inputLines.next().toInt).foreach(num => process(num, inputLines) (writer.println))
 		}
 		finally {
 			writer.flush()
@@ -28,26 +26,33 @@ object Codejam {
 
 	/*
 		문제풀이.
-		- 스테이지는 가로 r, 세로 c의 크기를 가진다.
-		- 스테이지의 각 바깥면은 반대쪽 면으로 이어지는 무한 구조.
-		 ( 3x3 스테이지일 경우 좌표 1,1 에서 좌측으로 이동하면 3,1이 된다.)
-		- 사과는 (r + c)가 홀수가 되는 좌표에 놓여진다. (먹으면 사라짐)
+		- game board는 가로 R, 세로 C의 크기를 가진다.
+		  좌상단의 좌표는 (1, 1)이 되고 우하단의 좌표는 (R, C)가 된다.
+		- 좌표 r + c가 홀수가 되는 cell에는 food가 놓여진다. ex: (1,2) (2,1) (2,3) (3, 2)
+		- game board의 각 바깥면은 반대쪽 면으로 순환된다.
+		  ( 3x3 스테이지일 경우 좌표 1,1 에서 좌측으로 이동하면 3,1이 된다.)
 		- Snake는 좌상단 (1,1) 좌표에서 우측을 바라본 상태에서 시작.
-		- 1초가 지날 때 마다 뱀은 뱀이 향하고 있는 방향으로 1칸 이동한다.
-		- 뱀이 사과를 먹으면 몸 길이가 1 증가한다.
+		- 1초가 지날 때 마다 뱀은 머리가 향하고 있는 인접 cell로 이동한다.
+		- 뱀의 머리가 먹이가 있는 cell로 이동하게 되면 음식을 먹고 (음식은 사라진다) body가 성장한다.
+		  새로운 head는 음식이 있던 cell에 생성되고 머리는 snake의 두번째 cell이 된다.
+		- player는 snake가 turn하는 action을 할 수 있다. 각 action Ai는 Ti와 Ti + 1 초 사이에 이루어진다.
+		  action은 두가지가 가능한데 head를 기준으로 L 방향으로 90회전, 혹은 R 방향으로 90도 회전이다.
 		- 게임은 뱀의 머리가 몸통에 부딪치거나 10^9 만큼의 시간이 지나면 끝난다.
 	 */
 
-	case class Point(x: Int, y: Int)
-	case class Command(second: Int, turn: String)
+	case class Cell(x: Int, y: Int)
+	case class Action(time: Int, turn: String)
 	case class Stage(rows: Int, columns: Int, snake: Snake) {
-		val array = Array.tabulate(rows, columns) ((x, y) => {
-			if ((x + y) % 2 == 0) Empty else Apple
-		})
+		val map = mutable.Map[Cell, Int]()
 
-		snake.body.foreach(b => array(b.x)(b.y) = Body)
+		for {
+			x <- 0 until rows
+			y <- 0 until columns
+		} if ( (x + y) % 2 == 0) map += (Cell(x,y) -> Empty) else map += (Cell(x,y) -> Apple)
+
+		snake.body.foreach(b => map(Cell(b.x, b.y)) = Body)
 	}
-	case class Snake(startDirection: (Int, Int), startBody: List[Point]) {
+	case class Snake(startDirection: (Int, Int), startBody: List[Cell]) {
 		var direction = startDirection
 		var body = startBody
 	}
@@ -55,29 +60,45 @@ object Codejam {
 	val LimitTime = Math.pow(10, 9).toLong
 	val Up = (0, -1); val Down = (0, 1)
 	val Left = (-1, 0); val Right = (1, 0)
-	val Empty = 0; val Apple = 1; val Body = 3
+	val Empty = 0; val Apple = 1; val Head = 10; val Body = 11 ;val Tail = 12
 
-	def process(inputLines: Iterator[String]): String = {
-		// 입력 값.
+	def process(num: Int, inputLines: Iterator[String])(lineOut: String => Unit): Unit = {
+		// 입력 값 parsing.
 		val Array(s, r, c) = inputLines.next().split(" ").map(_.toInt)
-		val commands = Queue[Command]() ++
-			(1 to s).map(_ => {inputLines.next().split(" ") match {case Array(t, d) => Command(t.toInt, d)}})
+		val actions = mutable.Queue[Action]() ++ (1 to s).map(
+			_ => inputLines.next().split(" ") match { case Array(t, d) => Action(t.toInt, d) } )
 
 		// 게임 초기화.
-		val snake = Snake(Right, List(Point(0, 0)))
-		val stage = Stage(r, c, snake)
+		val snake = Snake(Right, List(Cell(0, 0)))
+		val gameBoard = Stage(r, c, snake)
 		var elapseTime = 0
-		var command = commands.dequeue
-		var isGameOver = false
+		var isDead = false
 
-		def foward(point: Point): Unit = {
+		//---------------------------------------------------------------------------------------------//
+
+		def facingToward(point: Cell): Unit = {
+			val head = snake.body.head
 			val tail = snake.body.last
+
 			snake.body = (point :: snake.body).init
-			stage.array(tail.x)(tail.y) = Empty
-			stage.array(point.x)(point.y) = Body
+			gameBoard.map(tail) = Empty
+
+			val newTail = snake.body.last
+			gameBoard.map(point) = Head
+			gameBoard.map(head) = Body
+			gameBoard.map(newTail) = Tail
 		}
 
-		def turn(t: String) = t match {
+		def grow(point: Cell) = {
+			val head = snake.body.head
+			val tail = snake.body.last
+			snake.body = point :: snake.body
+			gameBoard.map(point) = Head
+			gameBoard.map(head) = Body
+			gameBoard.map(tail) = Tail
+		}
+
+		def turnAction(t: String) = t match {
 			case "L" => snake.direction = snake.direction match {
 				case Right => Up; case Up => Left
 				case Left => Down; case Down => Right
@@ -89,43 +110,45 @@ object Codejam {
 			case "N" => Unit
 		}
 
-		def grow() = snake.body = snake.body :+ snake.body.last
+		def gameTick(time: Int) = {
+			while (time > elapseTime && isDead == false) {
+				elapseTime += 1
 
-		do {
-			elapseTime += 1
+				// 이동 포인트 얻기.
+				val fowardPoint = {
+					val x = (snake.body.head.x + snake.direction._1) % r
+					val y = (snake.body.head.y + snake.direction._2) % c
+					Cell(if (x >= 0) x else r + x, if (y >= 0) y else c + y)
+				}
 
-			// 이동 포인트 얻기.
-			val point = {
-				val x = (snake.body.head.x + snake.direction._1) % r
-				val y = (snake.body.head.y + snake.direction._2) % c
-				Point(if(x >= 0) x else r + x, if (y >= 0) y else  c + y)
-			}
-
-			// 전진! 포인트에 사과 있으면 성장, 몸에 부딛히면 게임 오버.
-			stage.array(point.x)(point.y) match {
-				case Empty => foward(point)
-				case Apple => grow; foward(point)
-				case Body => isGameOver = true
-			}
-
-			// 명령을 내릴 시간이 됬다면~
-			if (command != null && command.second == elapseTime) {
-				// 뱀의 진행 방향을 변경.
-				turn(command.turn)
-				// 다음 명령 가져오기.
-				command = commands match {
-					case cs if (cs.isEmpty == false) => cs.dequeue;
-					case _ => null
+				// 전진! 포인트에 사과 있으면 성장, 몸에 부딛히면 게임 오버.
+				gameBoard.map(fowardPoint) match {
+					case Apple => grow(fowardPoint)
+					case Body => isDead = true
+					case _ => facingToward(fowardPoint) // Empty or Tail.
 				}
 			}
+		}
 
-			// 시간 만료 검사.
-			if (elapseTime >= LimitTime) isGameOver = true
+		//---------------------------------------------------------------------------------------------//
 
-		} while(isGameOver == false)
+		var action: Action = null
+		var isGrow = false
+
+		while (actions.isEmpty == false && isDead == false) {
+			action = actions.dequeue
+			gameTick(action.time)
+			turnAction(action.turn)
+		}
+
+		while (isGrow == false && isDead == false) {
+			val oldLength = snake.body.length
+			gameTick(elapseTime + oldLength)
+			isGrow = (snake.body.length - oldLength) == 0
+		}
 
 		// 결과.
-		return snake.body.length.toString
+		lineOut(s"Case #$num: ${snake.body.length.toString}")
 	}
 
 }
